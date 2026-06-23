2938c2220278bec9992fc30424b46a3a082f5532
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-06-23 12:18:05 +0545

    build: add compiled JavaScript output

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = 'Ram';
+console.log(name);
+let price = 100;
+console.log(price);
+let userOne = {
+    name: "Gyanendra",
+    age: 19,
+    address: {
+        temp: "sainamain",
+        perma: "butwal",
+        muni: {
+            ward: 12,
+            tole: "pulchowk"
+        }
+    }
+};
+console.log(userOne);
+let userListOne = [
+    {
+        name: "Gyanendra",
+        age: 19,
+        address: {
+            temp: "sainamain",
+            perma: "butwal",
+            muni: {
+                ward: 12,
+                tole: "pulchowk"
+            }
+        }
+    },
+    {
+        name: "Hari bahadur",
+        age: 66,
+        address: {
+            temp: "butwal",
+            perma: "sainamain",
+            muni: {
+                ward: 14,
+                tole: "pallo gaun"
+            }
+        }
+    }
+];
+// Tuple
+let n = [10, "Ram"];
+console.log(n);
+//# sourceMappingURL=index.js.map
